47c09ee9ac5a9e3e851cf6cd97cdd3be51caf7ff
Abil-Aub <59198773+Abil-Aub@users.noreply.github.com>
2021-11-20 22:14:53 +0100

    init php

---
+++ index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CV Abil</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width" initial-scale=1>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+	<script type="text/javascript" src="js/script.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="160">
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li><a href="#about">Me</a></li>
+					<li><a href="#skills">Competances</a></li>
+					<li><a href="#experience">Experience</a></li>
+					<li><a href="#education">Education</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#recommandations">Recommandations</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+
+		</div>
+	</nav>
+
+
+	<section id="about">
+		<div class="col-xs-6 col-md-3 profile-picture">
+			<img src="images/me.jpg" alt="Abil" class="img-circle">
+		</div>
+		<div class="heading">
+			<h1>Hello, it's me Abil</h1>
+			<h3>Developpeur Freelance | Junior</h3>
+			<a href="docs/CV.pdf" class="button1">Download CV</a>
+		</div>
+	</section>
+	
+
+	<section id="skills">
+		<div class="red-divider"></div>
+		<div class="heading">
+			<h2>Competances</h2>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+							<h5>HTML 85%</h5>
+						</div>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+							<h5>CSS 85%</h5>
+						</div>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%">
+							<h5>JAVASCRIPT 90%</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+							<h5>JQUERY 85%</h5>
+						</div>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%">
+							<h5>BOOTSTRAP 80%</h5>
+						</div>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:75%">
+							<h5>PYTHON 75%</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="experience">
+		<div class="container">
+			<div class="white-divider"></div>
+			<div class="heading">
+				<h2>Professional experience</h2>
+			</div>
+			<ul class="timeline">
+				<li>
+					<div class="timeline-badge"><span class="glyphicon glyphicon-briefcase"></span></div>
+					<div class="timeline-panel-container">
+						<div class="timeline-panel">
+							<div class="timeline-heading">
+								<h3>GOOGLE</h3>
+								<h4>Senior Web Developer</h4>
+								<p class="text-muted"><small class="glyphicon glyphicon-time"></small> 2013-2015</p>
+							</div>
+							<div class="timeline-body">
+								<p>Ajout de la possibilité d'écouter une traduction dans Google Translate</p>
+								<p>Développement de Google Suggest en mode Offline</p>
+								<p>Nouveau design du player de Youtube adapté au mobile</p>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="timeline-badge"><span class="glyphicon glyphicon-briefcase"></span></div>
+					<div class="timeline-panel-container-inverted">
+						<div class="timeline-panel">
+							<div class="timeline-heading">
+								<h3>FACEBOOK</h3>
+								<h4>Web Developer</h4>
+								<p class="text-muted"><small class="glyphicon glyphicon-time"></small> 2010-2013</p>
+							</div>
+							<div class="timeline-body">
+								<p>Développement du bouton Share pour les applis Web mobile</p>
+								<p>Lancement automatique des vidéos en mode mute depuis la Timeline</p>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="timeline-badge"><span class="glyphicon glyphicon-briefcase"></span></div>
+					<div class="timeline-panel-container">
+						<div class="timeline-panel">
+							<div class="timeline-heading">
+								<h3>TWITTER</h3>
+								<h4>Junior Web Developer</h4>
+								<p class="text-muted"><small class="glyphicon glyphicon-time"></small> 2007-2010</p>
+							</div>
+							<div class="timeline-body">
+								<p>Création et Développement du Retweet pour l'appli Web</p>
+								<p>Intégration des vidéos sur les applis web mobile</p>
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+
+		</div>
+	</section>
+
+
+	<section id="education">
+		<div class="container">
+			<div class="red-divider"></div>
+			<div class="heading">
+				<h2>Education</h2>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="education-block">
+						<h5>2002 - 2006</h5>
+						<span class="glyphicon glyphicon-education"></span>
+						<h3>Ecole Polytechnique - Paris</h3>
+						<h4>Diplome d'ingenieur informatique</h4>
+						<div class="red-divider"></div>
+						<p>Intelligence Artificielle</p>
+						<p>Systeme d'informations</p>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="education-block">
+						<h5>2007</h5>
+						<span class="glyphicon glyphicon-education"></span>
+						<h3>Apprendre-a-coder.com</h3>
+						<h4>Formation online "Web Developer"</h4>
+						<div class="red-divider"></div>
+						<p>HTML, CSS, Javascript, jQuery</p>
+						<p>Responsove Design</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="portfolio">
+		<div class="container">
+			<div class="white-divider"></div>
+			<div class="heading">
+				<h2>Portfolio</h2>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">
+					<a class="thumbnail" href="http://www.facebook.com" target="_blank">
+						<img src="images/facebook_share.png" alt="facebook share">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="thumbnail" href="http://www.twitter.com" target="_blank">
+						<img src="images/twitter_video.png" alt="twitter video">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="thumbnail" href="http://www.google.com" target="_blank">
+						<img src="images/google_translate.png" alt="google translate">
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">
+					<a class="thumbnail" href="http://www.youtube.com" target="_blank">
+						<img src="images/youtube.png" alt="youtube">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="thumbnail" href="http://www.twitter.com" target="_blank">
+						<img src="images/twitter_retweet.png" alt="twitter retweet">
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a class="thumbnail" href="http://www.facebook.com" target="_blank">
+						<img src="images/facebook_video.png" alt="facebook video">
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="recommandations">
+		<div class="container">
+			<div class="red-divider"></div>
+			<div class="heading">
+				<h2>Recommandations</h2>
+			</div>
+			<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="myCarousel" data-slide-to="1"></li>
+					<li data-target="myCarousel" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<h3>"John t'es le meilleur! Merci pour tout..."</h3>
+						<h4>Larry Page, Google Co-Founder</h4>
+					</div>
+					<div class="item">
+						<h3>"L'esprit le plus créatif que j'ai vu de toute ma vie..."</h3>
+						<h4>Jack Dorsey, Twitter Founder and CEO</h4>
+					</div>
+					<div class="item">
+						<h3>"Merci John de m'avoir appris à coder... Tout ça c'est grâce à toi!"</h3>
+						<h4>Mark Zuckerberg, Facebook Founder and CEO</h4>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#myCarousel" data-slide="prev" role="button">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next" role="button">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+
+			</div>
+		</div>
+	</section>
+
+
+	<section id="contact">
+       <div class="container">
+            <div class="white-divider"></div>
+            <div class="heading">
+                <h2>Contact</h2>
+            </div>
+                
+           <div class="row">
+               <div class="col-lg-10 col-lg-offset-1">
+                    <form id="contact-form" method="post" action="" role="form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="firstname">Prénom *</label>
+                                <input id="firstname" type="text" name="firstname" class="form-control" placeholder="Votre prénom">
+                                <p class="comments"></p>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="name">Nom *</label>
+                                <input id="name" type="text" name="name" class="form-control" placeholder="Votre Nom">
+                                <p class="comments"></p>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="email">Email *</label>
+                                <input id="email" type="text" name="email" class="form-control" placeholder="Votre Email">
+                                <p class="comments"></p>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="phone">Téléphone</label>
+                                <input id="phone" type="tel" name="phone" class="form-control" placeholder="Votre Téléphone">
+                                <p class="comments"></p>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="message">Message *</label>
+                                <textarea id="message" name="message" class="form-control" placeholder="Votre Message" rows="4"></textarea>
+                                <p class="comments"></p>
+                            </div>
+                            <div class="col-md-12">
+                                <p><strong>* Ces informations sont requises.</strong></p>
+                            </div>
+                            <div class="col-md-12">
+                                <input type="submit" class="button1" value="Envoyer">
+                            </div>    
+                        </div>
+                    </form>
+                </div>
+           </div>
+        </div>
+	</section>
+
+
+	<footer class="text-center">
+		<a href="#about">
+			<span class="glyphicon glyphicon-chevron-up"></span>
+		</a>
+		<h5>© 2021 APPRENDRE-A-CODER.COM</h5>
+	</footer>
+</body>
+</html>
+
+
+
