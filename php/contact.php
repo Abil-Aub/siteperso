6f20a0cf9b54e51210b3cc9e8c7ad7ba325f3b57
Abil-Aub <59198773+Abil-Aub@users.noreply.github.com>
2021-11-21 02:26:25 +0100

    Add files via upload

--- php/contact.php
+++ php/contact.php
@@ -65,30 +65,8 @@
         
         if($array["isSuccess"]) 
         {
-            // $headers = "From: {$array['firstname']} {$array['name']} <{$array['email']}>\r\nReply-To: {$array['email']}";
-            // mail($emailTo, "Un message de votre site", $emailText, $headers);
-            
-            $mail = new PHPMailer();
-
-            // Settings
-            $mail->IsSMTP();
-            $mail->CharSet = 'UTF-8';
-
-            $mail->Host       = "stmp.gmail.com";      // SMTP server example
-            $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-            $mail->SMTPAuth   = true;                  // enable SMTP authentication
-            $mail->Port       = 465;                   // set the SMTP port for the GMAIL server
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-            $mail->Username   = "aubakirov.abil@gmail.com";            // SMTP account username example
-            $mail->Password   = "Niveaformen85+";            // SMTP account password example
-
-            // Content
-            $mail->isHTML(true);                       // Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-            $mail->send();
+            $headers = "From: {$array['firstname']} {$array['name']} <{$array['email']}>\r\nReply-To: {$array['email']}";
+            mail($emailTo, "Un message de votre site", $emailText, $headers);
         }
         
         echo json_encode($array);
